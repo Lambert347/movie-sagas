7eb94c5fdc70ea533d461b7929e84281523c6d40
Ian Lambert <lambe347@umn.edu>
2021-05-16 11:37:10 -0500

    add movie component works and displays properly to the home page

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -7,10 +7,22 @@ function AddMovie(){
     const [title, setTitle] = useState('');
     const [poster, setPoster] = useState('');
     const [description, setDescription] = useState('');
-    const addMovie = () => {
+    const [genreId, setGenreId] = useState(0);
+    const genre = useSelector(store => store.genres);
+    const newMovie = {
+        title: title,
+        poster: poster,
+        description: description,
+        genre_id: genreId
+    }
+    const addMovie = (event) => {
+        event.preventDefault();
         dispatch({type: 'ADD_NEW_MOVIE', payload: newMovie})
     }
-    const genre = useSelector(store => store.genres);
+    const handleChange = (event) => {
+        this.setState({[event.target.genreId] : event.target.value})
+    }
+    
     useEffect(() => {
         dispatch({type: 'GET_GENRES'})
     }, [])
@@ -21,7 +33,7 @@ function AddMovie(){
             <input onChange={(event) => setTitle(event.target.value)} value={title} placeholder="Title of Movie"></input>
             <input onChange={(event) => setPoster(event.target.value)} value={poster} placeholder="Url for Movie Poster"></input>
             <textarea onChange={(event) => setDescription(event.target.value)} value={description} placeholder="Movie Description" />
-            <select>
+            <select value={genre.id} name='genreId' onChange={(event) => setGenreId(event.target.value)}>
                 {genre.map(genre => {
                     return <option key={genre.id} value={genre.id}>{genre.name}</option>
                 })}
