1e3e4671dba49cdbfed9d04d29d9be2c294681a6
Ian Lambert <lambe347@umn.edu>
2021-05-16 11:44:04 -0500

    pulled the addmovie function out of the home page and into its own route. Also added a navbar for navigation to that page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,24 @@ function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
+        <nav>
+            <div>
+              <Link to="/">Homepage</Link>
+            </div>
+            <div>
+              <Link to="/addmovie">Add a Movie</Link>
+            </div>
+        </nav>        
         <Route path="/" exact>
           <MovieList />
+        </Route>
+        <Route path="/addmovie" exact>
+          <AddMovie />
         </Route>
         
         {/* Details page */}
 
-        <AddMovie />
       </Router>
     </div>
   );
